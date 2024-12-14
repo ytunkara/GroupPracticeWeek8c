9794da4d8925b76a01f51343dd3f9429f85a9721
ytunkara <yuscheb@yahoo.com>
2024-12-14 17:19:06 -0500

    Week 8 Group Practice Using Git Bash Commit

---
+++ myproject/books/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from .models import Book
+
+def index(request):
+    books = Book.objects.all()
+    return render(request, 'books/index.html', {'books': books})
